c370e3a47d713e95f1df1086809eec3df1da7713
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-05-06 13:08:35 +0900

    [add] 子供向け広告の設定を追加

--- app/src/main/java/com/wsr/shopping_friend/MainActivity.kt
+++ app/src/main/java/com/wsr/shopping_friend/MainActivity.kt
@@ -3,6 +3,8 @@ package com.wsr.shopping_friend
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.RequestConfiguration.TAG_FOR_CHILD_DIRECTED_TREATMENT_TRUE
 import com.wsr.shopping_friend.databinding.ActivityMainBinding
 import com.wsr.shopping_friend.preference.checkVersion
 
@@ -16,6 +18,13 @@ class MainActivity : AppCompatActivity() {
         checkVersion(this)
 
         //広告
+        val requestConfiguration = MobileAds.getRequestConfiguration()
+            .toBuilder()
+            .setTagForChildDirectedTreatment(TAG_FOR_CHILD_DIRECTED_TREATMENT_TRUE)
+            .build()
+
+        MobileAds.setRequestConfiguration(requestConfiguration)
+
         val adRequest: AdRequest = AdRequest.Builder().build()
         ActivityMainBinding.inflate(layoutInflater).adView.loadAd(adRequest)
     }
